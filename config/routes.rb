977be92900bdb53b83275f42fd5f238f61f4db0d
pancakeCaptain <jaaulner@gmail.com>
2015-02-26 00:21:17 -0600

    minor comment & whitespace changes
    added comments to routes.rb and fixed indentation in
    application_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-#  devise_for :users
+  # leaving out devise's user routes so we can nest ticket routes below users
+  #devise_for :users
   root to: 'application#angular'
 
   resources :users, only: [:create, :update] do
     resources :tickets, only: [:create, :index, :destroy, :update]
   end
+
+  resources :tickets, only: [:index]
 end
