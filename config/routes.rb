9f6fd3fe3f8bfacd936433e2aedcbd53470dba81
pancakeCaptain <jaaulner@gmail.com>
2015-02-26 15:42:08 -0600

    Re-enabled devise routes
    re-enabled devise routes for users, nested ticket routes under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  # Disabling devise routes in order to define nested user & ticket routes
-  #devise_for :users
+  # Routing for our user accounts is managed by devise
+  devise_for :users
   root to: 'application#angular'
 
   # Define the routes we will need for creating and updating user accounts, and
   # nest routes for ticket creation, deletion, etc. below specific user routes.
-  resources :users, param: :username, only: [:create, :update] do
+  resources :users, only: [] do
     resources :tickets, only: [:create, :index, :destroy, :update]
   end
 
   # A separate route used to retrieve all tickets, because our previously
   # defined route for tickets:index is user-specific.
-  get 'tickets', to: 'ticket#index_all'
+  get '/tickets', to: 'ticket#index_all'
 end
