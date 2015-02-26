e26f50ddafbe8a3e8ab3563477e928b65c7eb4e7
pancakeCaptain <jaaulner@gmail.com>
2015-02-25 23:42:13 -0600

    Added user routes and ticket routes (nested). Generated ticket model and controller skeletons.

---
+++ app/controllers/ticket_controller.rb
@@ -0,0 +1,17 @@
+class TicketController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def ticket_params
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+#  devise_for :users
   root to: 'application#angular'
+
+  resources :users, only: [:create, :update] do
+    resources :tickets, only: [:create, :index, :destroy, :update]
+  end
 end
