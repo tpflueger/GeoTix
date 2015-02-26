13435cc0347f4b228aafd88f4fd129a00ec6ba24
pancakeCaptain <jaaulner@gmail.com>
2015-02-26 11:44:00 -0600

    Remove user from tickets table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226172619) do
+ActiveRecord::Schema.define(version: 20150226174217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20150226172619) do
   create_table "tickets", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
-    t.string   "user"
     t.boolean  "is_active"
     t.string   "lat"
     t.string   "long"
