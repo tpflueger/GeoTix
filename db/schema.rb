c420313c993085e2a9a889761e2445a835053176
pancakeCaptain <jaaulner@gmail.com>
2015-02-26 11:24:44 -0600

    Removed user_id from tickets table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226164148) do
+ActiveRecord::Schema.define(version: 20150226171707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20150226164148) do
     t.string   "long"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
