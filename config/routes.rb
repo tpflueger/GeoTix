3eaff6b6c150b5f13ba97ddef7223c2bf256585b
pancakeCaptain <jaaulner@gmail.com>
2015-02-26 01:12:51 -0600

    Revert "minor comment & whitespace changes"
    This reverts commit 977be92900bdb53b83275f42fd5f238f61f4db0d.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # leaving out devise's user routes so we can nest ticket routes below users
-  #devise_for :users
+#  devise_for :users
   root to: 'application#angular'
 
   resources :users, only: [:create, :update] do
     resources :tickets, only: [:create, :index, :destroy, :update]
   end
-
-  resources :tickets, only: [:index]
 end
