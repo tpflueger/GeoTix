c5e4e298056163877ff707098400907297ac868e
Tyler Pflueger <TylerPflueger@hotmail.com>
2015-03-07 12:09:15 -0600

    Remove unused ngDialog
    feature/refactor-appjs
    Remove unused dependency.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,9 +3,9 @@
 
   var app = angular.module('geotix', ['ui.router', 'templates', 'Devise', 'ngDialog', 'uiGmapgoogle-maps']);
 
-  app.run(['$rootScope', 'Auth', 'ngDialog', '$state', '$timeout', 'LoginService', controller]);
+  app.run(['$rootScope', 'Auth', '$state', '$timeout', 'LoginService', controller]);
 
-  function controller($rootScope, Auth, ngDialog, $state, $timeout, loginService) {
+  function controller($rootScope, Auth, $state, $timeout, loginService) {
     $rootScope.$state = $state;
     $rootScope.loginTry = true;
     $rootScope.registerTry = false;
