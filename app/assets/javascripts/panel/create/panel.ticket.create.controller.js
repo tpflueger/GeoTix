66a40feb202b58073cb20b4f99aa947935c729a0
pancakeCaptain <jaaulner@gmail.com>
2015-03-11 18:27:21 -0500

    Completed creation of new ticket objects
    Fixed submit() function call;
    Added lat/long to new ticket creation

--- app/assets/javascripts/panel/create/panel.ticket.create.controller.js
+++ app/assets/javascripts/panel/create/panel.ticket.create.controller.js
@@ -9,9 +9,14 @@
     $scope.newTicket = {};
 
     $scope.submit = function() {
-      console.log($scope.newTicket);
+      var ticket = {
+        lat: $scope.userCircle.center.latitude,
+        long: $scope.userCircle.center.longitude,
+        title: $scope.newTicket.title,
+        description: $scope.newTicket.description
+      };
 
-      ticketService.createUserTicket($scope.user.id, $scope.newTicket).then(function(data) {
+      ticketService.createUserTicket($scope.user.id, ticket).then(function(data) {
         $scope.userTickets.push(data);
       }, function(error) {
         console.log(error);
