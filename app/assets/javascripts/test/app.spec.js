40127de716e31efd4c6c5d11890cda1cf0c997c6
Tyler Pflueger <TylerPflueger@hotmail.com>
2015-04-06 13:11:14 -0500

    Add front-end tests setup
    feature/add-angular-tests
    Add setup for testing front-end.

---
+++ app/assets/javascripts/test/app.spec.js
@@ -0,0 +1,19 @@
+(function () {
+	'use strict';
+
+	describe('App', function () {
+		beforeEach(module('geotix'));
+
+		beforeEach(module(function ($provide) {
+
+		}));
+
+		beforeEach(inject(function (_$rootScope_, _$controller_, _$q_) {
+
+		}));
+
+		it("1", function () {
+			expect(0).toEqual(0);
+		});
+	});
+})();
