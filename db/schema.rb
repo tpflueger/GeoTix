eeffc682d80279834e6c6afe458fb2a1fc269d07
ChuntaoFu <cfu@my.bellevue.edu>
2015-04-15 18:57:25 -0500

    MyTickets section in user panel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226174217) do
+ActiveRecord::Schema.define(version: 20150306035309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,11 +20,11 @@ ActiveRecord::Schema.define(version: 20150226174217) do
     t.string   "title"
     t.string   "description"
     t.boolean  "is_active"
-    t.integer  "user_id"
     t.string   "lat"
     t.string   "long"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
