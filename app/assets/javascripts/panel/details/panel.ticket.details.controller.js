c09ccbfa856d5434e1ffe58b4cc20d8e983a6b29
pancakeCaptain <jaaulner@gmail.com>
2015-04-20 16:24:38 -0500

    Removed grab() and call from button
    Added _.find() to retrieve ticket when details state is loaded

--- app/assets/javascripts/panel/details/panel.ticket.details.controller.js
+++ app/assets/javascripts/panel/details/panel.ticket.details.controller.js
@@ -6,11 +6,11 @@
   app.controller('TicketDetailsController', ['$scope', ticketDetailsController]);
 
   function ticketDetailsController($scope) {
-    $scope.grab = function() {
-      var ticketId = $scope.$state.params.ticket_id;
-      var ticket = {};
-
-      console.log(ticketId);
+    $scope.initialize = function() {
+      $scope.ticketId = parseInt($scope.$state.params.ticket_id, 10);
+      $scope.ticket = _.find($scope.userTickets, { id: $scope.ticketId });
     };
+
+    $scope.initialize();
   };
 })();
