de3ad9eee975fd67327ab0fb36019213c02552dd
pancakeCaptain <jaaulner@gmail.com>
2015-04-21 09:08:39 -0500

    Created details panel
    Added details of selected ticket to the details panel

--- app/assets/javascripts/panel/details/panel.ticket.details.controller.js
+++ app/assets/javascripts/panel/details/panel.ticket.details.controller.js
@@ -9,6 +9,9 @@
     $scope.initialize = function() {
       $scope.ticketId = parseInt($scope.$state.params.ticket_id, 10);
       $scope.ticket = _.find($scope.userTickets, { id: $scope.ticketId });
+      console.log("ticketId: " + $scope.ticketId);
+      console.log($scope.ticket);
+      console.log($scope.userTickets);
     };
 
     $scope.initialize();
